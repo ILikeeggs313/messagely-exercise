a3c5f2c610c48c501be905df3a931a00e87dc9ef
anhnguyen <kyanhnguyen0502@gmail.com>
2023-02-16 18:49:24 -0500

    initial part 3, have not added getting messages for users yet

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,44 @@
+const Router = require('express').Router;
+const router = new Router();
+const jwt = require('jsonwebtoken');
+const ExpressError = require('../expressError');
+const {SECRET_KEY} = require('../config');
+//lastly expressError
+const ExpressError = require('../expressError');
+const User = require('../models/user');
+const {ensureLoggedIn, ensureCorrectUser} = require('../middleware/auth');
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+//they first have to be logged in
+router.get('/', ensureLoggedIn, async (req, res, next) => {
+    try{let users = await User.all();
+    return res.json({users});
+    } catch(e){
+        return next(e)
+    }
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
+router.get('/:username', ensureCorrectUser, async (req, res, next) => {
+    try{    
+        let user = await User.get(req.params.id);
+        if (!user){
+            throw new ExpressError(`No user found`, 401);
+        }
+        return res.json({user});
+        //if no user just throw an error
+        
+    } catch (e){
+        return next(e)
+    }
+})
 
 /** GET /:username/to - get messages to user
  *
